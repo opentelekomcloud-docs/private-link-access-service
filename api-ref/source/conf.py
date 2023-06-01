090ad7813d936f4fe08768372b0089e161206f75
tischrei <tino.schreiber@t-systems.com>
2023-06-01 08:27:26 +0000

    Update tox.ini && conf.py file
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -28,6 +28,15 @@ project = 'Private Link Access Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/private-link-access-service'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/private-link-access-service/api-ref/'
+otcdocs_doc_title = 'API Reference'
+otcdocs_doc_type = 'api-ref'
+otcdocs_service_category = 'network'
+otcdocs_service_title = 'Private Link Access Service'
+otcdocs_service_type = 'plas'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
